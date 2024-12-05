dbfd7f8a3a52eb8912bd582837175b0b2543582a
DELL <DELL@192.168.0.196>
2024-12-05 13:19:23 +0530

    TL--developed main method

--- gitproj1/src/com/ms/dao/Abc.java
+++ gitproj1/src/com/ms/dao/Abc.java
@@ -3,6 +3,10 @@ package com.ms.dao;
 public class Abc {
 	public static void main(String[] args)
 {
-		System.out.println("assalamu alaikum");
+		//System.out.println("assalamu alaikum");
+		XYZ 	ob1= new XYZ();
+		ob1.m1();
 }
+	//calling method1 developed by developer 1
 }
+	
