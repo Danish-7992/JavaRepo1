fb5a530ae078ec0b3bedc2e6aa656c998f653223
DELL <DELL@192.168.0.196>
2024-12-05 17:57:20 +0530

    TL--payment branch2

--- gitproj1/src/com/ms/dao/XYZ.java
+++ gitproj1/src/com/ms/dao/XYZ.java
@@ -6,7 +6,7 @@ public class XYZ {
 
 		System.out.println("Method 1 Developed for Learning");
         F1 ob= new F1();
-    int result=    ob.m1(12, 23);
+    int result=    ob.m1(12, 23); 
     System.out.println("Result : "+result);
 	}
 } 
