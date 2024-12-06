9469ba9291e30e287b2fec4ead961e7c4e84aa02
DELL <DELL@192.168.0.196>
2024-12-06 11:40:04 +0530

    DEv1--stash commit

--- gitproj1/src/com/ms/dao/k2.java
+++ gitproj1/src/com/ms/dao/k2.java
@@ -3,4 +3,9 @@ package com.ms.dao;
 public class k2 {
 //assalamu alaikum
 	
+	public String disp() {
+		
+		return "git hub zindabad";
+	}
+	
 }
--- gitproj1/src/com/ms/dao/m1.java
+++ gitproj1/src/com/ms/dao/m1.java
@@ -5,4 +5,9 @@ public class m1 {
 	public void hello() {
 		System.out.println("Hello A");
 	}
+	
+	public void hello2() {
+		System.out.println("Hello B");
+	}
 }
+
